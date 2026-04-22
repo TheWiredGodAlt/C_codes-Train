325ae2d5bbb426e3273d34b9b276c78eab10e72a
TheWiredGodAlt <wiredgd66@gmail.com>
2026-04-22 11:31:18 -0300

    first switch function scenario

--- hello.c
+++ hello.c
@@ -1,26 +1,18 @@
-/*My first C code for development*/
 #include<stdio.h>
-#include<string.h>
-/*My first C hello world*/
 int main(void){
-    int grade,count,total,med;
-    const char* sent="n";
-    char strinput[1];
-    total=0;
-    count=0;
-    while(strcmp(sent,strinput)!=0){
-        printf("Add a new grade(y=yes n=no):\n");
-        scanf(" %c",&strinput);
-            printf("Insert your bimestral grade\n");
-            scanf(" %f",&grade);
-            count++;
-            total=grade+total;
-    };
-    med=total/count;
-    printf("The med value is:%f",med);
+    int grade;
+    int a_count=0;
+    printf("Hello World, please insert your grade in american format \n(EOF for ending the process)");
+
+    while((grade=getchar())!= EOF){
+        switch(grade){
+
+            case 'A':
+                ++a_count;
+                    break;
+        }
+
+    }
+    printf("\nThe number of A's is:%d",a_count);
     return(0);
 }
-/*End of my first C Hello world main function, simple addition*/
-
-
-
