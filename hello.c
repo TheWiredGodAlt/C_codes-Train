219ef711526cb809ef05a254bc4805b49b471ffb
TheWiredGodAlt <wiredgd66@gmail.com>
2026-04-22 12:08:59 -0300

    Second attempt

--- hello.c
+++ hello.c
@@ -3,14 +3,20 @@ int main(void){
 
 //basic variables
     int grade;
-    int a_count=0;
+    int a_count=0, b_count=0;
     printf("Hello World, please insert your grade in american format \n(EOF for ending the process)");
 
 //Get char function, takes a character and puts it inside an int type variable 
     while((grade=getchar())!= EOF){
         switch(grade){
+//Basic case functions, with focus on counting the number of A's and B's
             case 'A':
+            case 'a':
                 ++a_count;
+                    break;
+            case 'B':
+            case 'b':
+                    ++b_count;
                     break;
 
 //Prevents the default message from looping
@@ -20,11 +26,12 @@ int main(void){
                     break;
 //Makes easier for declaring case variables without repeating 
             default:
-                    printf("Wrong letter try again:");
+                    printf("Wrong letter try again:\n");
                     break;
         }
 
     }
     printf("\nThe number of A's is:%d",a_count);
+    printf("\nThe number of B's is:%d",b_count);
     return(0);
 }
