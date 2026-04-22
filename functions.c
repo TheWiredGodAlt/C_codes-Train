6ff3319ead77487782e541cbf0073ae65b101e1a
TheWiredGodAlt <wiredgd66@gmail.com>
2026-04-22 17:15:46 -0300

    first homemade function

--- functions.c
+++ functions.c
@@ -1,7 +1,18 @@
+//Basic train code about functions and math library.
 #include<stdio.h>
 #include<math.h>
+int square(int y);
 int main(void){
-    printf("%2.f",sqrt(900));
-
+//Basic square root calculus made using sqrt function from the math lib. 
+    printf("basic usage for the math lib %.2f\n",sqrt(900));
+    int x;
+    printf("Hello, and welcome to our first function test\n");
+    for(x=1;x<=10;x++){
+        printf("%d\n", square(x), "\n");
+    }
     return(0);
+}
+//Ending of Main function.
+int square(int y){
+    return y*y;
 }
