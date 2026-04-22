462fe5e0c02eb842d1d8be9e52ab62688138d2af
TheWiredGodAlt <wiredgd66@gmail.com>
2026-04-22 13:10:10 -0300

    basic difference code

--- dowhile_break_continue.c
+++ dowhile_break_continue.c
@@ -1,12 +1,25 @@
 #include<stdio.h>
 int main(void){
-    int counter=0;
+    int counter=-1;
     printf("Simple counter:\n");
     do{
-        printf("%d",counter);
+        counter;
         counter++;
+        if (counter==5){
+            continue;
+        }
+//In this scenario, the computer ignores the value of 5, jumps to 6, and prints until 11 (it ignores the value of the first memory position too)
+        printf("%d",counter);
     }while(counter<=10);
 
-
+//The difference between the continue function is visible here.
+//This code is gonna show just the normal sequence, ignoring the 5, without any value jumps.
+    printf("\nSimple counter 2:\n");
+    for(counter=0;counter<=10;counter++){
+        if(counter==5){
+            continue;
+        }
+        printf("%d",counter);
+    };
     return(0);
 }
