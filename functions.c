ee5745f58b9f563990b12b9d9ec81370a0a93ba2
TheWiredGodAlt <wiredgd66@gmail.com>
2026-04-22 18:02:20 -0300

    more function understanding

--- functions.c
+++ functions.c
@@ -1,18 +1,18 @@
 //Basic train code about functions and math library.
 #include<stdio.h>
 #include<math.h>
-int square(int y);
-int main(void){
-//Basic square root calculus made using sqrt function from the math lib. 
-    printf("basic usage for the math lib %.2f\n",sqrt(900));
-    int x;
-    printf("Hello, and welcome to our first function test\n");
-    for(x=1;x<=10;x++){
-        printf("%d\n", square(x), "\n");
-    }
-    return(0);
-}
-//Ending of Main function.
+//Making and the homemade function.
 int square(int y){
     return y*y;
 }
+int main(void){
+//Basic square root calculus made using sqrt function from the math lib. 
+    printf("basic usage for the math lib %.2f\n",sqrt(900));
+    printf("Hello, and welcome to our first function test\n");
+    int x;
+    for(x=0;x<=10;x++){
+        printf("%d\n", square(x));
+    }
+    return(0);
+}
+
