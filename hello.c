dcff0d34b0f9a8e433208f70685a2ebd70873d61
TheWiredGodAlt <wiredgd66@gmail.com>
2026-04-22 12:02:31 -0300

    My first commit without cheating

--- hello.c
+++ hello.c
@@ -1,14 +1,26 @@
 #include<stdio.h>
 int main(void){
+
+//basic variables
     int grade;
     int a_count=0;
     printf("Hello World, please insert your grade in american format \n(EOF for ending the process)");
 
+//Get char function, takes a character and puts it inside an int type variable 
     while((grade=getchar())!= EOF){
         switch(grade){
-
             case 'A':
                 ++a_count;
+                    break;
+
+//Prevents the default message from looping
+            case '\n':
+            case '\t':
+            case ' ':
+                    break;
+//Makes easier for declaring case variables without repeating 
+            default:
+                    printf("Wrong letter try again:");
                     break;
         }
 
