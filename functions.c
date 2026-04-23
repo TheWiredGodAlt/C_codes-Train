c672521394b6efa9c02c3571116311328fba76d3
TheWiredGodAlt <wiredgd66@gmail.com>
2026-04-22 23:25:06 -0300

    complete understanding of more complex functions

--- functions.c
+++ functions.c
@@ -1,10 +1,37 @@
 //Basic train code about functions and math library.
 #include<stdio.h>
 #include<math.h>
-//Making and the homemade function.
+
+//Making the first homemade function.
 int square(int y){
     return y*y;
 }
+//"Complex" function prototype(just a reference so the compiler won't bug)
+int max(int a, int b, int c);
+int main(void){
+//Var used by our first homemade function
+    int x;
+
+//the variables used by the "max" function
+    int number1, number2, number3;
+
+//Basic square root calculus made using sqrt function from the math lib. 
+    printf("basic usage for the math lib %.2f\n",sqrt(900));
+
+//Our first homemade function test
+    printf("Hello, and welcome to our first function test\n");
+//Logic for printing the numeric sequence for our first homemade function
+    for(x=0;x<=10;x++){
+        printf("%d\n", square(x));
+    };
+//Basicc scanf of the the variables that are gonna be used by the function
+    printf("Hello again, and welcome to the second homemade function learning test\nPlease insert three interger numbers:\n");
+    scanf("%d %d %d",&number1,&number2,&number3);
+//Calling the actual function and giving it it's value
+    printf("The biggest integer is:%d",max(number1,number2,number3));
+    return(0);
+}
+//The whole math logic beeing executed by the "max" function
 int max(int a, int b, int c){
     int max_v=a;
      if(b>max_v){
@@ -15,20 +42,3 @@ int max(int a, int b, int c){
     }
     return max_v;
 }
-int main(void){
-//Basic square root calculus made using sqrt function from the math lib. 
-    printf("basic usage for the math lib %.2f\n",sqrt(900));
-    printf("Hello, and welcome to our first function test\n");
-    int x;
-//the variables used in the second homemade function test
-    int number1, number2, number3;
-    for(x=0;x<=10;x++){
-        printf("%d\n", square(x));
-    };
-//Basicc scanf of the the variables that are gonna be used by the function
-    printf("Hello again, and welcome to the second homemade function learning test\nPlease insert three interger numbers");
-    scanf("%d %d %d",&number1,&number2,&number3);
-    printf("The biggest integer is %d",max(number1,number2,number3));
-    return(0);
-}
-
